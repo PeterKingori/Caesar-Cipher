e4b8f7e9418364b0a62ec9923b0f9637b7820c03
Peter Kingori <kpndegwa@gmail.com>
2020-01-24 23:44:02 +0300

    Add logic in CaesarCipher.java to save the user input string provided in the constructor to a member variable named 'private String userInput' in that class. Also set this attribute with the value passed in the constructor. Then return the member variable upon request using the 'getInputString()' method.

--- src/main/java/models/CaesarCipher.java
+++ src/main/java/models/CaesarCipher.java
@@ -1,6 +1,13 @@
 package models;
 
 public class CaesarCipher {
+    private String userInput;
+
     public CaesarCipher(String userInput, Integer shiftKey) {
+        this.userInput = userInput;
+    }
+
+    public String getInputString() {
+        return this.userInput;
     }
 }
--- src/test/java/models/CaesarCipherTest.java
+++ src/test/java/models/CaesarCipherTest.java
@@ -5,8 +5,14 @@ import static org.junit.Assert.*;
 
 public class CaesarCipherTest {
     @Test
-    public void newCaesarCipher_instantiatesCorrectly() {
+    public void newCaesarCipher_instantiatesCorrectly() throws Exception {
         CaesarCipher testCaesarCipher = new CaesarCipher("A", 1);
         assertEquals(true, testCaesarCipher instanceof CaesarCipher);
+    }
+
+    @Test
+    public void newCaesarCipher_getsString_A() {
+        CaesarCipher testCaesarCipher = new CaesarCipher("A", 1);
+        assertEquals("A", testCaesarCipher.getInputString());
     }
 }
