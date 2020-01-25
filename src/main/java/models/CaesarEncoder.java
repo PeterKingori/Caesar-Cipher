8a0311c67ce097f3fa62f8c8c53c6aa487c8e592
Peter Kingori <kpndegwa@gmail.com>
2020-01-25 14:32:28 +0300

    Edit the CaesarEncoder.java file by first adding a private member variable to hold a string value of user input. A new object of type CaesarCipher from CaesarCipher.java is also created. Logic is added to CaesarEncoderInput() method to get the input string from the CaesarCipher.java class and save it in the member variable in CaesarEncoder.java.

--- src/main/java/models/CaesarEncoder.java
+++ src/main/java/models/CaesarEncoder.java
@@ -1,6 +1,14 @@
 package models;
 
 public class CaesarEncoder {
-    public CaesarEncoder(String receivedUserInput, int receivedKey) {
+    private String receivedUserInput;
+    CaesarCipher encodeCaesarCipher = new CaesarCipher("", 0);
+
+    public CaesarEncoder(String receivedUserInput, int receivedKey) { }
+
+    public String CaesarEncoderInput() {
+        receivedUserInput = encodeCaesarCipher.getInputString();
+        this.receivedUserInput = receivedUserInput;
+        return this.receivedUserInput;
     }
 }
--- src/test/java/models/CaesarEncoderTest.java
+++ src/test/java/models/CaesarEncoderTest.java
@@ -5,14 +5,15 @@ import static org.junit.Assert.*;
 
 public class CaesarEncoderTest {
     @Test
-    public void newCaesarEncoder_instantiatesCorrectly() {
+    public void newCaesarEncoder_instantiatesCorrectly() throws Exception {
         CaesarEncoder testCaesarEncoder = new CaesarEncoder("A", 1);
         assertEquals(true, testCaesarEncoder instanceof CaesarEncoder);
     }
-}
 
-//    @Test
-//    public void getInputStringFromCaesarCipherClass_getsString_String() {
-//        CaesarEncoder testCaesarEncoder = new CaesarEncoder("A", 1);
-//        assertEquals("A", testCaesarEncoder.receivedInputString());
-//    }
+
+    @Test
+    public void getInputStringFromCaesarCipherClass_getsString_String() throws Exception {
+        CaesarEncoder testCaesarEncoder = new CaesarEncoder("", 1);
+        assertEquals("", testCaesarEncoder.CaesarEncoderInput());
+    }
+}
