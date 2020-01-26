4b7591b812e1944eddd24fcc8c859a54f4f64f2c
Peter Kingori <kpndegwa@gmail.com>
2020-01-27 00:36:20 +0300

    Add an option in App.java so that a user chooses if they want to encrypt or decrypt text.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,16 +8,28 @@ import java.io.InputStreamReader;
 public class App {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("I am a Caesar Cipher program. Enter the text you want to be " +
-                    "encrypted:");
-        String inputText = bufferedReader.readLine();
-        System.out.println("Enter an integer that you want to use as the encoding key:");
-        String stringInputKey = bufferedReader.readLine();
-        int inputKey = Integer.parseInt(stringInputKey);
-
-        CaesarCipherEncoder caesarCipherEncoder = new CaesarCipherEncoder(inputText, inputKey);
-        String encodingResult = caesarCipherEncoder.encryptUserText();
-        System.out.println("Encrytped Text: " + encodingResult);
-
+        System.out.println("I am a Caesar Cipher program. Choose one of the following options: " +
+                "Encrypt or Decrypt");
+        String choice = bufferedReader.readLine();
+        if(choice.equals("Encrypt")) {
+            System.out.println("Enter the text that you want to be encrypted:");
+            String userInputText = bufferedReader.readLine();
+            System.out.println("Enter an integer that you want to use as the encoding key:");
+            String stringInputKey = bufferedReader.readLine();
+            int inputKey = Integer.parseInt(stringInputKey);
+            CaesarCipherEncoder caesarCipherEncoder = new CaesarCipherEncoder(userInputText,inputKey);
+            String encodingResult = caesarCipherEncoder.encryptUserText();
+            System.out.println("Encrytped Text: " + encodingResult);
+        } else if (choice.equals("Decrypt")) {
+            System.out.println("Enter the text that you want to be decrypted:");
+            String userInputText = bufferedReader.readLine();
+            System.out.println("Enter an integer that you want to use as the decoding key:");
+            String stringInputKey = bufferedReader.readLine();
+            int inputKey = Integer.parseInt(stringInputKey);
+            CaesarCipherDecoder caesarCipherDecoder = new CaesarCipherDecoder(userInputText,
+                    inputKey);
+            String decodingResult = caesarCipherDecoder.decodeUserText();
+            System.out.println("Encrytped Text: " + decodingResult);
+        }
     }
 }
